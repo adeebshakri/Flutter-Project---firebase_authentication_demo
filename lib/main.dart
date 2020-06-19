0a1e8a86c45f56bbf307ff9197de82b048f3ee46
adeebshakri_ <adeebshakri7@gmail.com>
2020-06-19 20:40:39 +0530

    First commit for firebase autehntication

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_database/ui/firebase_animated_list.dart';
-import 'board.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final FirebaseAuth _auth = FirebaseAuth.instance;
+final GoogleSignIn _googleSignIn = new GoogleSignIn();
+
 
 void main() {
   runApp(MyApp());
@@ -11,38 +14,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Community Board',
+      title: 'Auth - Firebase',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: new MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-
+class MyHomePage extends StatefulWidget{
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<StatefulWidget> createState() => new _MyHomePage();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  List<Board> boardMessages = List();    //a list of objects Board , boardMessages is used to add all the board objects we are retrieving and send to our db
-  Board board; //a Board object
-  final FirebaseDatabase database = FirebaseDatabase.instance; //handle to get in our firebase db (a db reference/ a db object)
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();//it will help us to get a form key handle to use
-  DatabaseReference databaseReference; // databaseReference object
-
-  @override
-  void initState() {
-    super.initState();
-
-    board = Board("", "");  //instantiate board
-    databaseReference = database.reference().child("community_board"); //creating a tree , child is what we ill be adding to our realtime db
-    databaseReference.onChildAdded.listen(_onEntryAdded); //allows us to have a callback from db .listen will continuously be listening to changes being added onto db
-    databaseReference.onChildChanged.listen(_onEntryChanged);
-  }
+class _MyHomePage extends State<MyHomePage>{
+String _imageUrl;
 
   @override
   Widget build(BuildContext context) {
@@ -50,104 +38,104 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: Text("Community board"),
         ),
-        body: Column(
-          children: <Widget>[
-            Flexible(
-              flex: 0,
-              child: Center(
 
-                child: Form(
-                  key: formKey,
-                  child: Flex(
-                    direction: Axis.vertical,
-                    children: <Widget>[
-                      ListTile(
-                        leading: Icon(Icons.subject),
-                        title: TextFormField(
-                          initialValue: "",
-                          onSaved: (val) => board.subject = val,  //setting board instance field of the object to whatever we are passing
-                          validator: (val) => val == "" ? val : null,
-                        ),
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.message),
-                        title: TextFormField(
-                          initialValue: "",
-                          onSaved: (val) => board.body = val,
-                          validator: (val) => val == "" ? val : null,
-                        ),
-                      ),
+        body:Center(
+            child:Column(
+              mainAxisAlignment: MainAxisAlignment.center ,
+              children: <Widget>[
 
-                      //Send or Post button
-                      FlatButton(
-                        child: Text("Post"),
-                        color: Colors.greenAccent,
-                        onPressed: () {
-                          handleSubmit();
-                        },
-                      )
-
-                    ],
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(                    // Shortcut:  click on flatbutton and Alt+Enter to add padding
+                    child: Text("Google-Signin"),
+                    onPressed: () => _gSignin(),
+                    color: Colors.red,
                   ),
                 ),
-              ),),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                    child: Text("Signin with Email"),
+                    onPressed: () => _signInWithEmail(),
+                    color: Colors.yellow,
 
-            //to show dynamically all of the board communications we are adding into our db to be shown as a Listview in the UI
-            Flexible(
-                child: FirebaseAnimatedList(      // we could have also gone by creating a Listview Builder. FirebaseAnimatedList is a Listview Builder that has all the things we need to query a firebase db and list all the items in a Listview
-                  query: databaseReference,      //query will hold the data from our db . We get that from passing in the datbaseReference as such
-                  itemBuilder: (_, DataSnapshot snapshot, // Because FirebaseAnimatedList is a Listview , thus itemBuilder. _ is context
-                      Animation<double> animation, int index) {
-                    return Card(
-                      child: ListTile(
-                        leading: CircleAvatar(
-                          backgroundColor: Colors.red,
-                        ),
-                        title: Text(boardMessages[index].subject),
-                        subtitle: Text(boardMessages[index].body),
-                      ),
-                    );
-                  },
-                )
-            ),
-          ],
-        )
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                    child: Text("Create Account"),
+                    onPressed: () => _createUser(),
+                    color: Colors.purple,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                    color: Colors.red,
+                    child: Text("Signout"),onPressed: ()=> _signout(),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: FlatButton(
+                    color: Colors.teal,
+                    child: Text("Signout of Email"),onPressed: ()=> _signoutOfEmail(),
+                  ),
+                ),
+                Image.network(_imageUrl == null || _imageUrl.isEmpty ? "https://tse1.mm.bing.net/th?id=OIP.6TahjUtodiFzyOyfxKYsKAHaHC&pid=Api&P=0&w=100&h=95":_imageUrl)
+              ],))
+
     );
   }
 
-  void _onEntryAdded(Event event) {
-    setState(() {          //setState will go ahead and rebuild the UI
-      boardMessages.add(Board.fromSnapshot(event.snapshot));
-    });
-  }
 
-  void handleSubmit() {
-    final FormState form = formKey.currentState;
-    if (form.validate()) {   //if all our fields have data
-      form.save(); //save
-      form.reset(); //clear out
-      //save form data to the db
-      databaseReference.push().set(board.toJson()); //push() gives each item we add into our db a very unique key
-    }
-  }
+  Future<FirebaseUser> _gSignin() async{  //google sign in
+    GoogleSignInAccount googleSignInAccount = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
 
-  void _onEntryChanged(Event event) {
-    var oldEntry = boardMessages.singleWhere((entry) {  //singleWhere is used to compare element we want to compare
-      return entry.key == event.snapshot.key;
-    });
+    final AuthCredential credential = GoogleAuthProvider.getCredential(   //Stackoverflow
+      idToken: googleSignInAuthentication.idToken,
+      accessToken: googleSignInAuthentication.accessToken,
+    );
+    final AuthResult authResult = await _auth.signInWithCredential(credential);
+    FirebaseUser user = authResult.user;
+
+    print("User is: " + user.photoUrl);
     setState(() {
-      boardMessages[boardMessages.indexOf(oldEntry)] = Board.fromSnapshot(event.snapshot);
+      _imageUrl = user.photoUrl;
+    });
+    return user;
+  }
+
+  Future _createUser()async {  //create new account
+    FirebaseUser user  = (await FirebaseAuth.instance.createUserWithEmailAndPassword(email: "sampleemail2@gmail.com", password: "test12345")).user ; //stackoverflow
+      print("User created ${user.displayName}");
+      print("Email: ${user.email}");
+
+    }
+    _signout(){
+    setState(() {
+      _googleSignIn.signOut();
+      _imageUrl = null;
+    });
+    }
+
+  _signInWithEmail() async { //stsckoverflow
+
+      FirebaseUser user = (await _auth.signInWithEmailAndPassword(email: "sampleemail2@gmail.com", password: "test12345")
+          .catchError((err){
+        print("Something went wrong! ${err.toString()}");
+      })).user;
+    print("User signed in: ${user.email}");
+  }
+
+  _signoutOfEmail() {
+    setState(() {
+      _auth.signOut();
+      _imageUrl = null;
     });
   }
 }
 
-
-
-//database.reference().child("message").set({   //reading from db
-//   "firstname": "Adeeb"
-//});
-//setState((){
-//  database.reference().child("message").once().then((DataSnapshot snapshot){
-//    Map<dynamic,dynamic> data = snapshot.value;
-//    print("Values from db: ${snapshot.value}");
-//});
+//This is only for Android Firebase
